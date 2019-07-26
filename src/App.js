75d52e15a02713dc3f40a0fc262af9e484fccdde
max-sindi <britva.okkamaa@gmail.com>
2019-07-26 11:33:48 +0300

    small comment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import './App.css'
 import axios from 'axios'
 
+// TODO: it would be nice to create .env file and move it there
 axios.defaults.baseURL = 'http://localhost:8000/api'
 
 class App extends Component {
